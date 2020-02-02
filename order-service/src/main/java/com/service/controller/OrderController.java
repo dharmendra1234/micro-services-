1d778cb4a0c2ebb26307ee910562e2b9c13b1b18
Dharmendra Pawar <dharmendra.pawar@gmail.com>
2020-02-01 20:57:56 -0500

    feign client

--- order-service/src/main/java/com/service/controller/OrderController.java
+++ order-service/src/main/java/com/service/controller/OrderController.java
@@ -2,15 +2,36 @@ package com.service.controller;
 
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.service.dao.OrderDao;
+import com.service.entity.Order;
 @RestController
+@JsonIgnoreProperties(ignoreUnknown = true)
 @RequestMapping("/order")
 public class OrderController {
+	
+	@Autowired
+	OrderDao orderDao;
     
     @GetMapping("/status/check")
     public String status() {
         return "Order service is up and running!!";
     }
+    
+	
+    @GetMapping("/orders")
+    public List<Order> getOrderList() {
+    	return this.orderDao.list();
+    	// return "Hello adasd";
+    }
+	 
+
+    
 }
